b6715265b404d19cb34147e8a879aaf1866f3d41
shota-watanabe <t8mmmxc11y@yahoo.co.jp>
2025-07-31 14:45:36 +0900

    設定ファイルの修正

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,15 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import { includeIgnoreFile } from "@eslint/compat";
-import tsEslintParser from "@typescript-eslint/parser";
-import eslintReactiveValueSuffix from "eslint-plugin-reactive-value-suffix";
-import globals from "globals";
-import vueEslintParser from "vue-eslint-parser";
-import withNuxt from "./.nuxt/eslint.config.mjs";
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { includeIgnoreFile } from '@eslint/compat';
+import tsEslintParser from '@typescript-eslint/parser';
+import eslintReactiveValueSuffix from 'eslint-plugin-reactive-value-suffix';
+import globals from 'globals';
+import vueEslintParser from 'vue-eslint-parser';
+import withNuxt from './.nuxt/eslint.config.mjs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const gitignorePath = path.resolve(__dirname, ".gitignore");
+const gitignorePath = path.resolve(__dirname, '.gitignore');
 
 export default withNuxt([
   includeIgnoreFile(gitignorePath),
@@ -18,44 +18,31 @@ export default withNuxt([
       parser: vueEslintParser,
       parserOptions: {
         parser: tsEslintParser,
-        project: path.resolve(__dirname, "./tsconfig.json"),
-        extraFileExtensions: [".vue"],
+        projectService: true,
+        tsconfigRootDir: __dirname,
+        extraFileExtensions: ['.vue'],
       },
       globals: {
         ...globals.browser,
         ...globals.node,
       },
     },
-    ignores: ["*.mjs", "*.ts"],
-    plugins: { "reactive-value-suffix": eslintReactiveValueSuffix },
+    ignores: ['*.mjs', '*.ts'],
+    plugins: { 'reactive-value-suffix': eslintReactiveValueSuffix },
     rules: {
-      "vue/html-self-closing": "off",
-      "vue/multi-word-component-names": "off",
-      "import/order": [
-        "warn",
+      'vue/html-self-closing': 'off',
+      'vue/multi-word-component-names': 'off',
+      'import/order': [
+        'warn',
         {
-          groups: [
-            "builtin",
-            "external",
-            "internal",
-            "parent",
-            "sibling",
-            "index",
-            "object",
-            "type",
-          ],
-          pathGroups: [
-            { pattern: "@/**", group: "parent", position: "before" },
-          ],
-          pathGroupsExcludedImportTypes: ["builtin"],
-          alphabetize: { order: "asc" },
-          "newlines-between": "never",
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
+          pathGroups: [{ pattern: '@/**', group: 'parent', position: 'before' }],
+          pathGroupsExcludedImportTypes: ['builtin'],
+          alphabetize: { order: 'asc' },
+          'newlines-between': 'never',
         },
       ],
-      "reactive-value-suffix/suffix": [
-        "error",
-        { functionNamesToIgnoreValueCheck: [] },
-      ],
+      'reactive-value-suffix/suffix': ['error', { functionNamesToIgnoreValueCheck: [] }],
     },
   },
 ]);
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,5 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   srcDir: 'src/',
-  modules: ['@nuxt/eslint', '@nuxt/test-utils']
-})
+  modules: ['@nuxt/eslint', '@nuxt/test-utils'],
+});
