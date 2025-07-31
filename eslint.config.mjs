2758069d0398e436b33d96d9a0900abf6838831f
shota-watanabe <t8mmmxc11y@yahoo.co.jp>
2025-07-31 14:51:27 +0900

    Vitestの設定

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,8 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { includeIgnoreFile } from '@eslint/compat';
 import tsEslintParser from '@typescript-eslint/parser';
+import vitest from '@vitest/eslint-plugin';
+import vuePrettierConfig from '@vue/eslint-config-prettier';
 import eslintReactiveValueSuffix from 'eslint-plugin-reactive-value-suffix';
 import globals from 'globals';
 import vueEslintParser from 'vue-eslint-parser';
@@ -45,4 +47,26 @@ export default withNuxt([
       'reactive-value-suffix/suffix': ['error', { functionNamesToIgnoreValueCheck: [] }],
     },
   },
+  {
+    files: ['**/__test__/**', '**/__tests__/**', '**/tests/**', '**/test/**'],
+    plugins: {
+      vitest,
+    },
+    rules: {
+      ...vitest.configs.recommended.rules,
+      'vitest/consistent-test-it': ['error', { fn: 'test', withinDescribe: 'test' }],
+      'vitest/no-conditional-expect': 'error',
+      'vitest/no-conditional-in-test': 'error',
+      'vitest/no-conditional-tests': 'error',
+      'vitest/no-disabled-tests': 'error',
+      'vitest/no-duplicate-hooks': 'error',
+      'vitest/no-identical-title': 'error',
+      'vitest/no-test-return-statement': 'error',
+      'vitest/prefer-mock-promise-shorthand': 'error',
+      'vitest/require-hook': 'error',
+      'vitest/require-to-throw-message': 'error',
+      'vitest/require-top-level-describe': ['error', { maxNumberOfTopLevelDescribes: 2 }],
+    },
+  },
+  vuePrettierConfig,
 ]);
