38062a74c236af129c6ba3f727fd3bac44b1d4b7
shota-watanabe <t8mmmxc11y@yahoo.co.jp>
2025-07-31 14:18:07 +0900

    Prettier設定追加

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,61 @@
-// @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { includeIgnoreFile } from "@eslint/compat";
+import tsEslintParser from "@typescript-eslint/parser";
+import eslintReactiveValueSuffix from "eslint-plugin-reactive-value-suffix";
+import globals from "globals";
+import vueEslintParser from "vue-eslint-parser";
+import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt(
-  // Your custom configs here
-)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
+
+export default withNuxt([
+  includeIgnoreFile(gitignorePath),
+  {
+    languageOptions: {
+      parser: vueEslintParser,
+      parserOptions: {
+        parser: tsEslintParser,
+        project: path.resolve(__dirname, "./tsconfig.json"),
+        extraFileExtensions: [".vue"],
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    ignores: ["*.mjs", "*.ts"],
+    plugins: { "reactive-value-suffix": eslintReactiveValueSuffix },
+    rules: {
+      "vue/html-self-closing": "off",
+      "vue/multi-word-component-names": "off",
+      "import/order": [
+        "warn",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+            "object",
+            "type",
+          ],
+          pathGroups: [
+            { pattern: "@/**", group: "parent", position: "before" },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          alphabetize: { order: "asc" },
+          "newlines-between": "never",
+        },
+      ],
+      "reactive-value-suffix/suffix": [
+        "error",
+        { functionNamesToIgnoreValueCheck: [] },
+      ],
+    },
+  },
+]);
